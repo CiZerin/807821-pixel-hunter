4062c38c8f3804f0616e4b310113e635414ea39a
Artem Baulin <abaulin@gmail.com>
2018-11-19 19:27:46 +0300

    add navigation arrows

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const templates = document.querySelectorAll('template');
-const main = document.getElementById('main');
+const templates = document.querySelectorAll(`template`);
+const main = document.getElementById(`main`);
 let active;
 
 //Выводит контент шаблона на экран
 const showContent = (tempNum) => {
   const temp = templates[tempNum];
   const clon = temp.content.cloneNode(true);
-  main.innerHTML = '';
+  main.innerHTML = ``;
   main.appendChild(clon);
   active = tempNum;
 }
@@ -15,7 +15,7 @@ const showContent = (tempNum) => {
 showContent(1);
 
 //Листание экранов кнопками
-document.addEventListener('keydown', (evt) => {
+document.addEventListener(`keydown`, (evt) => {
   if (evt.keyCode === 37) { // ArrowLeft
     showContent(active-1);
   }
@@ -23,3 +23,30 @@ document.addEventListener('keydown', (evt) => {
     showContent(active+1);
   }
 })
+
+//Добавляем стрелки навигации
+const arrows = document.createElement(`div`);
+arrows.classList.add(`arrows__wrap`);
+document.body.appendChild(arrows);
+const arrowsStyle = document.createElement(`style`);
+arrows.appendChild(arrowsStyle);
+arrowsStyle.innerHTML = `.arrows__wrap {
+  position: absolute;
+  top: 95px;
+  left: 50%;
+  margin-left: -56px;
+}
+.arrows__btn {
+  background: none;
+  border: 2px solid black;
+  padding: 5px 20px;
+}`
+
+const arrowLeft = document.createElement('button');
+arrowLeft.classList.add(`arrows__btn`);
+arrowLeft.innerHTML = `<-`;
+arrows.appendChild(arrowLeft);
+const arrowRight = document.createElement(`button`);
+arrowRight.classList.add(`arrows__btn`);
+arrowRight.innerHTML = `->`;
+arrows.appendChild(arrowRight);
