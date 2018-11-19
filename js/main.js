e04f826bd6b913a250d334b4d5302a09ed03dff5
Artem Baulin <abaulin@gmail.com>
2018-11-19 20:36:35 +0300

    syntex and style fixes according eslint test

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
+'use strict';
 const templates = document.querySelectorAll(`template`);
 const main = document.getElementById(`main`);
 let active;
 
-//Выводит контент шаблона на экран
+// Выводит контент шаблона на экран
 const showContent = (tempNum) => {
   const temp = templates[tempNum];
   const clon = temp.content.cloneNode(true);
   main.innerHTML = ``;
   main.appendChild(clon);
   active = tempNum;
-}
+};
 
-//Установка начального экрана
+// Установка начального экрана
 showContent(1);
 
-//Листание экранов кнопками
+// Листание экранов кнопками
 document.addEventListener(`keydown`, (evt) => {
   if (evt.keyCode === 37) { // ArrowLeft
-    showContent(active-1);
+    showContent(active - 1);
+  } else if (evt.keyCode === 39) { // ArrowRight
+    showContent(active + 1);
   }
-  else if (evt.keyCode === 39) { // ArrowRight
-    showContent(active+1);
-  }
-})
+});
 
-//Добавляем стрелки навигации
+// Добавляем стрелки навигации
 const arrows = document.createElement(`div`);
 arrows.classList.add(`arrows__wrap`);
 document.body.appendChild(arrows);
@@ -40,9 +40,9 @@ arrowsStyle.innerHTML = `.arrows__wrap {
   background: none;
   border: 2px solid black;
   padding: 5px 20px;
-}`
+}`;
 
-const arrowLeft = document.createElement('button');
+const arrowLeft = document.createElement(`button`);
 arrowLeft.classList.add(`arrows__btn`);
 arrowLeft.innerHTML = `<-`;
 arrows.appendChild(arrowLeft);
@@ -52,8 +52,8 @@ arrowRight.innerHTML = `->`;
 arrows.appendChild(arrowRight);
 
 arrowLeft.addEventListener(`click`, () => {
-  showContent(active-1);
-})
+  showContent(active - 1);
+});
 arrowRight.addEventListener(`click`, () => {
-  showContent(active+1);
-})
+  showContent(active + 1);
+});
