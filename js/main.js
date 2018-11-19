44e737dcdd03f6f25183fb24f3cddebb4e9d6a07
Artem Baulin <abaulin@gmail.com>
2018-11-19 18:41:58 +0300

    add function for template render in #main screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 const templates = document.querySelectorAll('template');
+const main = document.getElementById('main');
+
+//Выводит контент шаблона на экран
+const showContent = (tempNum) => {
+  const temp = templates[tempNum];
+  const clon = temp.content.cloneNode(true);
+  main.innerHTML = '';
+  main.appendChild(clon);
+}
