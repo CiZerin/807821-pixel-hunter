6f5a476651d4b0ddf7c6827807eb937438e1bf67
Artem Baulin <abaulin@gmail.com>
2018-11-19 19:04:02 +0300

    add navigation between screens by keyboard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const templates = document.querySelectorAll('template');
 const main = document.getElementById('main');
+let active;
 
 //Выводит контент шаблона на экран
 const showContent = (tempNum) => {
@@ -7,6 +8,18 @@ const showContent = (tempNum) => {
   const clon = temp.content.cloneNode(true);
   main.innerHTML = '';
   main.appendChild(clon);
+  active = tempNum;
 }
 
+//Установка начального экрана
 showContent(1);
+
+//Листание экранов кнопками
+document.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 37) { // ArrowLeft
+    showContent(active-1);
+  }
+  else if (evt.keyCode === 39) { // ArrowRight
+    showContent(active+1);
+  }
+})
